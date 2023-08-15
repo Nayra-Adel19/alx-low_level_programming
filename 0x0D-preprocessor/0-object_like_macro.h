faeebfedd6502ee8ce0c0fc4d53106f7095d83c9
Nayra-Adel19 <nayra.adel19@gmail.com>
2023-08-15 16:01:07 -0700

    :D lol :D

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,9 +1,7 @@
 #ifndef _OBJECT_LINK_MACRO_H
 #define _OBJECT_LINK_MACRO_H
 
-/**
- * Create a header file that defines a macro named
- */
+/* SIZE as an abbreviation for the token 1024 */
 
 #define SIZE 1024
 
